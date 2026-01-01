ceea5f67613a20ea93914e7eca05ebccb9c7d71c
Digvijay Devkar <digvijaydevkar2325@gmail.com>
2026-01-01 11:28:32 +0530

    the whole project

--- backend/db/pool.js
+++
@@ -1,9 +0,0 @@
-const mysql2 = require('mysql2');
-
-const pool = mysql2.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Digvijay@2005',
-    database: 'int_courses'
-});
-module.exports = pool;
--- backend/utils/config.js
+++
@@ -1,3 +0,0 @@
-const SECRET = 'aasdfghjklqwertyuiefjksnzcgd'
-
-module.exports = {SECRET}
